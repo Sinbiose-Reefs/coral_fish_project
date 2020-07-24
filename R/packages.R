bc770dab71348de37e1285e4d604da74ab76fa2b
andreluza <luza.andre@gmail.com>
2020-07-24 11:37:33 -0300

    Adicionei alguns pacotes

--- R/packages.R
+++ R/packages.R
@@ -9,5 +9,11 @@ require(rnaturalearth)
 require(rnaturalearthdata)
 require(ggplot2)
 require(ggrepel)
+
+## pacotes para carregar os dados
 require(xlsx)
 require(openxlsx)
+
+## pacote para colar dados em arrays
+require(abind)
+
