d55fd663a8e6abfa563dad367a085f30aa250004
andreluza <luza.andre@gmail.com>
2020-07-24 14:28:59 -0300

    .R para chamar os pacotes necessarios para a modelagem da ocupação de sitios por corais

---
+++ R/packages_model_coral.R
@@ -0,0 +1,15 @@
+## pacotes para vizinhanca
+require(rgdal)
+require(raster)
+require(rgeos)
+require (spdep) 
+
+## pacote para transitar entre pastas
+require(here)
+
+## pacote para processamento paralelo
+require(parallel)
+
+# pacote para chamar o winbugs
+require(R2WinBUGS)
+
